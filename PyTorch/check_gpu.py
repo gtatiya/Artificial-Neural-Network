52507840350f8ce3e3c287bb94fb06e8d1ee6d0c
Gyan Tatiya <gtatiya@live.com>
2023-06-22 17:37:34 -0400

    Added PyTorchLightning

---
+++ PyTorch/check_gpu.py
@@ -0,0 +1,19 @@
+import torch
+
+print('torch.cuda.is_available()', torch.cuda.is_available())
+print('torch.cuda.device_count()', torch.cuda.device_count())
+print('torch.cuda.current_device()', torch.cuda.current_device())
+print('torch.cuda.device(0)', torch.cuda.device(0))
+print('torch.cuda.get_device_name(0)', torch.cuda.get_device_name(0))
+
+# setting device on GPU if available, else CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+print()
+
+#Additional Info when using cuda
+if device.type == 'cuda':
+    print(torch.cuda.get_device_name(0))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
